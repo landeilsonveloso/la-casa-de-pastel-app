ac0b606d35b5cfdae3092e6455a0525ef8c48683
landeilsonveloso <landeilsonveloso@gmail.com>
2025-08-18 16:55:57 -0300

    ajustes

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
-export default function Select({className, value, onChange, children, required}) {
+export default function Select({className, value, onChange, children}) {
     return (
-        <select className={className} value={value} onChange={onChange} required={required}>
+        <select className={className} value={value} onChange={onChange} required>
             {children}
         </select>    
     )
